f588e58652d3a30688409755e09bd6adc2e2c9f0
Tariq Daouda <tariq.daouda@umontreal.ca>
2019-06-17 11:53:50 -0400

    onnx kibd for label name

--- Spyrkle/pages/graphs_more.py
+++ Spyrkle/pages/graphs_more.py
@@ -3,7 +3,7 @@ from .graph_pages import GraphCrawler
 class pyTorchCrawler(GraphCrawler):
     """docstring for pyTorchCrawler"""
     
-    def __init__(self, model, inputs, ignore_nodes=None):
+    def __init__(self, model, inputs, ignore_nodes=None, remove_nameless_scopes=True):
         import torch
         self.trace, out = torch.jit.get_trace_graph(model, inputs)
         torch.onnx._optimize_trace(self.trace, torch.onnx.OperatorExportTypes.ONNX)
@@ -19,17 +19,24 @@ class pyTorchCrawler(GraphCrawler):
         else :
             self.ignore_nodes = ignore_nodes
 
+        self.remove_nameless_scopes = remove_nameless_scopes
+
     def get_next(self, node) :
         return [o.node() for o in node.inputs()]
 
+    def _should_ignore(self, scopeName) :
+        last = scopeName.split("/")[-1]
+        return last in self.ignore_nodes
+
     def get_node_uid(self, node) :
-        # if len(node.scopeName()) == 0 or self.ignore_nodes :
-        if self.ignore_nodes :
+        scopeName = node.scopeName()
+        if (self.remove_nameless_scopes and len(scopeName) == 0) or (self._should_ignore(scopeName) ) :
             return None
-        return node.scopeName() + ">(" + "-".join([o.uniqueName() for o in node.outputs()]) + ")"
+        return scopeName + ">(" + "-".join([o.uniqueName() for o in node.outputs()]) + ")"
     
     def get_node_label(self, node) :
-        base_name = node.scopeName()[len(self.model.__class__.__name__)+1:]
+        base_name = node.kind()[6:]#[len(self.model.__class__.__name__)+1:]
+        # print(node.kind(), base_name)
         if len(base_name) == 0 :
             base_name = node.kind()[len("onnx::"):]
         return base_name
